ff13e356fd49b2e99e7fe40d5fc2fe4c9d5c3a3c
Nixhazel <neolokor1@gmail.com>
2023-04-04 09:58:07 +0100

    added local files to repository

---
+++ src/DBmodels/transactionModel.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema(
+	{
+		transactionType: {
+			type: String,
+			required: true
+		},
+		status: {
+			type: String
+		},
+		passengerId: {
+			type: String,
+			required: true
+		},
+		amount: {
+			type: Number,
+			required: true
+		},
+		processed: {
+			type: Boolean,
+			default: false
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+
+const transactionModel = mongoose.model('transactions', transactionSchema);
+
+export default transactionModel;
---
+++ src/routes/users.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import {
+	createUser,
+	loginController,
+	changePassword,
+	bookTrip,
+	engagePayment,
+	getReference,
+	transactionHistory,
+	getAllPassengerTrips
+} from '../controller/users';
+
+import {
+	getPasswordController,
+	requestPasswordResetController,
+	resetPasswordController
+} from '../utils/passwordReset';
+import { updateVerified } from '../utils/updateVerified';
+import { authenticateToken } from '../middleware/auth';
+
+const router = express.Router();
+
+router.put('/changepassword', authenticateToken, changePassword);
+
+router.post('/signup', createUser);
+router.post('/login', loginController);
+router.get('/emailval/:id/:token', updateVerified);
+
+router.post('/forgot-password', requestPasswordResetController);
+router.get('/get-password/:userId/:token', getPasswordController);
+router.put('/reset-password/:id', resetPasswordController);
+
+router.post('/book-trip/:routeId', authenticateToken, bookTrip);
+router.get(
+	'/transaction-history',
+	authenticateToken,
+	transactionHistory
+);
+
+router.get('/getAllPassengerTrips', authenticateToken, getAllPassengerTrips)
+
+router.post('/paystack/pay', authenticateToken, engagePayment);
+router.get('/paystack/callback', authenticateToken, getReference);
+
+export default router;
